a1098578f6e41f9e259a421a54ed6528534b5b07
eriabe <g13901ae+git@gm.tsuda.ac.jp>
2015-05-27 17:16:42 +0900

    eclipse版１

---
+++ src/moomin/MoominStandServlet.java
@@ -0,0 +1,11 @@
+package moomin;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class MoominStandServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
